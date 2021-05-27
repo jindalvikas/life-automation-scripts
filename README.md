c3601b9533c87783f3e4a364e08e729f1158a6d6
Vaibhav Sawant <vaibhav.sawant@turtlemint.com>
2021-05-27 16:51:05 +0530

    Updated generateRiderInterdependencyRules.py

---
+++ README.md
@@ -0,0 +1,9 @@
+### Use Python 3 to Run the automation scripts
+
+### View help Text for any script using (-h) command
+Example:
+python <script_name> -h
+
+### Run Validation Script using following arguments:
+Example:
+python generateRiderValidationRules.py **-f** /Users/vaibhavsawant/Desktop/RiderValidationRules.csv **-o** ./life_rider_validations_mashreq.java **-b** turtlemint
--- generateRiderValidationRules.py
+++ generateRiderValidationRules.py
@@ -1,14 +1,40 @@
 import csv
 
+
+import argparse
+
+# Initialize parser
+parser = argparse.ArgumentParser()
+# EXAMPLE
+# python2 generateRiderValidationRules.py -f /Users/vaibhavsawant/Desktop/RiderValidationRules.csv -o ./life_rider_validations_mashreq.java -b turtlemint
+
+# Adding optional argument
+parser.add_argument("-f", "--file_path", help="Input File Path")
+parser.add_argument("-o", "--output_file", help="Output File Path")
+parser.add_argument("-b", "--broker", help="Broker")
+
+
+# Read arguments from command line
+args = parser.parse_args()
+
+if args.file_path:
+    print("file_path: % s" % args.file_path)
+
+if args.output_file:
+    print("output_file: % s" % args.output_file)
+
+if args.broker:
+    print("broker: % s" % args.broker)
+
 # Below is the script for Validation rules
 
-f1 = open('./life_rider_validations_mashreq.java', 'w+')
+f1 = open(args.output_file, 'w+')
 
-with open('/Users/vikas/Desktop/RiderValidationRules_mashreq.csv', mode='r') as csv_file:
+with open(args.file_path, mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     s12 = ' ' * 12
 
-    ruleNameText = 'rule "Mashreq Rider Validation Rule - {0}, {1}, {2}, {3}"\n'
+    ruleNameText = 'rule "{4} Rider Validation Rule - {0}, {1}, {2}, {3}"\n'
 
     whenRuleText = """    when
         riderRequest:RiderRequest(
@@ -23,7 +49,7 @@ with open('/Users/vikas/Desktop/RiderValidationRules_mashreq.csv', mode='r') as 
             riderRequest.getRiderPolicyTerm() <= riderRequest.getBasePlanPolicyTerm() &&
             riderRequest.getRiderPremiumPaymentTerm() <= riderRequest.getBasePlanPremiumPaymentTerm() &&{13}
             riderRequest.getCurrency() == "{14}" &&
-            riderRequest.getBroker() == "mashreq" &&
+            riderRequest.getBroker() == "{15}" &&
             riderRequest.getRuleId() == "validateRiders"
         )
 """
@@ -46,13 +72,13 @@ end
         else:
             riderSALessThanCheck = ""
 
-        ruleName = ruleNameText.format(row["riderCode"], row["productCode"], row["optionCode"], row["currency"])
+        ruleName = ruleNameText.format(row["riderCode"], row["productCode"], row["optionCode"], row["currency"], args.broker.capitalize())
 
         whenRule = whenRuleText.format(
             row["riderCode"], row["productCode"], row["optionCode"], row["minEntryAge"], row["maxEntryAge"],
             row["minMaturityAge"], row["maxMaturityAge"], row["MinPolicyTerm"], row["MaxPolicyTerm"],
             row["MinPremiumPaymentTerm"], row["maxPremiumPaymentTerm"], row["MinSumAssured"], row["MaxSumAssured"],
-            riderSALessThanCheck, row["currency"]
+            riderSALessThanCheck, row["currency"], args.broker
         )
 
         print >> f1, (ruleName + whenRule + thenResponse)
